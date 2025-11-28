c3a4488afa71cf3fbd3be6922007bc683f7bd9b4
marat30pipo-blip <marat30pipo@gmail.com>
2025-11-28 21:34:08 +0300

    создали первых пользователей

---
+++ src/main/java/Messenger.java
@@ -0,0 +1,15 @@
+public class Messenger {
+    final int MAX_FRIENDS = 15;
+
+    User arslanov = new User("Арсланов" , "мехмат святой", "мужской", new User[MAX_FRIENDS], "все на комсу");
+    User valiulin = new User("Марат" , "20551635", "мужской", new User[MAX_FRIENDS], "я тупой");
+    User gallamov = new User("Артур" , "51484", "мужской", new User[MAX_FRIENDS], "я сплю");
+    User zubkova = new User("Светлана Константиновна" , "4785568", "женский", new User[MAX_FRIENDS], "добрая душа");
+    User enikeev = new User("Еникеев Камиль Шамилевич" , "5416546", "мужской", new User[MAX_FRIENDS], "веселый, жизнерадостный");
+    User kalimullin = new User("Калимуллин" , "дискра и язык си", "мужской", new User[MAX_FRIENDS], "я вас спасу");
+
+
+
+
+    Group[] groups = {};
+}
